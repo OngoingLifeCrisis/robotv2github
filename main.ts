b656a06f0f90bf0237e9da4fdfb0a1493380b511
OngoingLifeCrisis <88060971+OngoingLifeCrisis@users.noreply.github.com>
2021-08-01 16:14:44 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,14 +6,13 @@ radio.onReceivedNumber(function (receivedNumber) {
     // lower the plates
     qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, -45, 0)
     qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, -45, 0)
-    // 0 means not following line. 1 means follow line
+    control.waitMicros(1000000)
+    qdee.qdee_setMotorSpeed(50, 50)
+    control.waitMicros(1000000)
+    qdee.qdee_setMotorSpeed(0, 0)
     Line_action_state = receivedNumber
-    control.waitMicros(2000000)
-    // 0 means not following line. 1 means follow line
-    sensing_state = 1
 })
 // set up robot and move servos to start position
-let sensing_state = 0
 let Line_action_state = 0
 qdee.qdee_Init()
 qdee.qdee_init_colorSensor(qdee.colorSensorPort.port4)
@@ -21,13 +20,24 @@ qdee.qdee_init_colorSensor(qdee.colorSensorPort.port4)
 radio.setGroup(39)
 // 0 means not following line. 1 means follow line
 Line_action_state = 0
-// 0 means not following line. 1 means follow line
-sensing_state = 1
 // index 1 and 2 are the names of our servos.
 // 
 // set the starting position of plate. Raised
 qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, 20, 0)
 qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, -120, 0)
+basic.forever(function () {
+    while (Line_action_state == 0) {
+        if (qdee.qdee_readLineFollowerStatus(qdee.lineFollowPort.port1, qdee.qdee_lineFollower.S1_IN_S2_OUT)) {
+            qdee.qdee_setMotorSpeed(0, 0)
+        } else if (qdee.qdee_readLineFollowerStatus(qdee.lineFollowPort.port1, qdee.qdee_lineFollower.S1_OUT_S2_IN)) {
+            qdee.qdee_setMotorSpeed(0, 0)
+        } else if (qdee.qdee_readLineFollowerStatus(qdee.lineFollowPort.port1, qdee.qdee_lineFollower.S1_IN_S2_IN)) {
+            qdee.qdee_setMotorSpeed(0, 0)
+        } else if (qdee.qdee_readLineFollowerStatus(qdee.lineFollowPort.port1, qdee.qdee_lineFollower.S1_OUT_S2_OUT)) {
+            qdee.qdee_setMotorSpeed(0, 0)
+        }
+    }
+})
 basic.forever(function () {
     while (Line_action_state == 1) {
         if (qdee.qdee_readLineFollowerStatus(qdee.lineFollowPort.port1, qdee.qdee_lineFollower.S1_IN_S2_OUT)) {
@@ -41,40 +51,26 @@ basic.forever(function () {
         }
     }
 })
-// for when it reaches the station
 basic.forever(function () {
-    if (Line_action_state == 1) {
-        if (sensing_state == 1) {
-            if (qdee.qdee_checkCurrentColor(qdee.qdee_Colors.Blue)) {
-                // sensing state=1 means it is checking whether it has reached the stall
-                sensing_state = 0
-                // sensing state=1 means it is checking whether it has reached the stall
-                Line_action_state = 0
-                // raise the plates
-                qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, 19, 0)
-                qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, -120, 0)
-            }
-        }
-    }
-})
-// for when it reaches the stall
-basic.forever(function () {
-    if (Line_action_state == 1) {
-        if (sensing_state == 1) {
-            if (qdee.qdee_checkCurrentColor(qdee.qdee_Colors.Red)) {
-                // sensing state=1 means it is checking whether it has reached the stall
-                sensing_state = 0
-                // sensing state=1 means it is checking whether it has reached the stall
-                Line_action_state = 0
-                // raise the plates
-                qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, -46, 0)
-                qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, -45, 0)
-            }
-        }
-    }
-})
-basic.forever(function () {
-    if (Line_action_state == 0) {
+    if (qdee.qdee_checkCurrentColor(qdee.qdee_Colors.Green)) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        // warning beeps
+        music.playTone(262, music.beat(BeatFraction.Whole))
         qdee.qdee_setMotorSpeed(0, 0)
+        // index 1 and 2 are the names of our servos.
+        // 
+        // set the starting position of plate. Raised
+        qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, 20, 0)
+        qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, -120, 0)
+    } else if (qdee.qdee_checkCurrentColor(qdee.qdee_Colors.Red)) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        // warning beeps
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        qdee.qdee_setMotorSpeed(0, 0)
+        // index 1 and 2 are the names of our servos.
+        // 
+        // set the starting position of plate. Raised
+        qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, 20, 0)
+        qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, -120, 0)
     }
 })
